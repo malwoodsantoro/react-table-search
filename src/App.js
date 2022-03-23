f5a54ce7bfd2b39dfd610a0a666a50dcdefa5a41
mal-wood <mallorywood413@gmail.com>
2022-03-22 21:26:27 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [searchInput, setSearchInput] = React.useState('');
+  const [filteredResults, setFilteredResults] = useState([]);
+
+  useEffect(() => {
+    fetch("./data/data.json")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json[0].movies);
+      });
+  });
+
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue)
+    if (searchInput !== '') {
+      const filteredData = data.filter((item) => {
+        return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+      })
+      setFilteredResults(filteredData)
+    }
+    else {
+      setFilteredResults(data)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        placeholder='Search for a movie...'
+        onChange={(e) => searchItems(e.target.value)}
+      />
+      <table class="table-styled">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Year</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {searchInput.length > 1 ? (
+            filteredResults.map((item, i) => {
+              return (
+                <tr key={i}>
+                  <td>{item.name}</td>
+                  <td>{item.year}</td>
+                  <td>{item.rating}</td>
+                </tr>
+              );
+            })
+          ) : (
+            data.map((item, i) => {
+              return (
+                <tr key={i}>
+                  <td>{item.name}</td>
+                  <td>{item.year}</td>
+                  <td>{item.rating}</td>
+                </tr>
+              );
+            })
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
